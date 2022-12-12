7f149ba7b7bb2b6195be6c3e4d3f93a1543d1d33
Sơn Lê Minh <lmson1989@gmail.com>
2022-12-12 17:32:28 +0700

    init firebase tracking

--- NemoSDK/NemoSDKTracking.xcframework/ios-arm64/NemoSDKTracking.framework/Headers/NemoSDKTracking.h
+++ NemoSDK/NemoSDKTracking.xcframework/ios-arm64/NemoSDKTracking.framework/Headers/NemoSDKTracking.h
@@ -6,12 +6,17 @@
 //
 #import <UIKit/UIKit.h>
 #import <NemoSDKTracking/AppsflyerManager.h>
+#import <NemoSDKTracking/FirebaseManager.h>
 
 @interface NemoSDKTracking : NSObject{
 }
 
 + (NemoSDKTracking *)sharedInstance;
 + (AppsflyerManager *)AppsFlyer;
++ (FirebaseManager *)Firebase;
+
 - (void)initTracking:(UIApplication *)application;
+- (BOOL)applicationDelegate:(id)appDelegate andApplication:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
 
 @end
